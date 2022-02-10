77b60a46a09ac354e57a3e6897d1db980ac8be7d
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2022-02-10 18:21:27 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Worker } from "@react-pdf-viewer/core";
+// Import the main component
+import { Viewer } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+// Import styles
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
+// Import the styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pdfFile, setPdfFile] = useState(null);
+	const [pdfError, setPdfError] = useState("");
+
+	// creating new plugin instance
+	const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+	// handle file onChange event
+	const allowedFiles = ["application/pdf"];
+	const handleFile = (e) => {
+		let selectedFile = e.target.files[0];
+		// console.log(selectedFile.type);
+		if (selectedFile) {
+			if (selectedFile && allowedFiles.includes(selectedFile.type)) {
+				let reader = new FileReader();
+				reader.readAsDataURL(selectedFile);
+				reader.onloadend = (e) => {
+					setPdfError("");
+					setPdfFile(e.target.result);
+					console.log(e.target.result);
+				};
+			} else {
+				setPdfError("PDF Only");
+			}
+		} else {
+			console.log("please select a PDF");
+		}
+	};
+	return (
+		<div className="container">
+			<form>
+				<label>
+					<h5>Upload PDF</h5>
+				</label>
+				<br />
+				{pdfError && <span className="text-danger">{pdfError}</span>}
+				<br />
+				<input type="file" className="form-control" onChange={handleFile} />
+			</form>
+			<br />
+			<h5>View PDF</h5>
+			<div className="viewer">
+				{pdfFile && (
+					<Worker workerUrl="https://unpkg.com/pdfjs-dist@2.12.313/build/pdf.worker.min.js">
+						<Viewer fileUrl={pdfFile} plugins={[defaultLayoutPluginInstance]} />
+					</Worker>
+				)}
+				{!pdfFile && <>No File Is Selected Yet</>}
+			</div>
+		</div>
+	);
 }
 
 export default App;
